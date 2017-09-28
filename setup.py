c804c8fbcc6af46caef019b95249ac594265dd57
ankur <ankurj630@gmail.com>
2017-09-28 11:31:32 +0530

    version 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="django_rest_guardian",
 
     # Version number (initial):
-    version="1.0.0",
+    version="1.0.1",
 
     # Application author details:
     author="Ankur Jain",
@@ -13,17 +13,12 @@ setup(
 
     # Packages
     packages=["django_rest_guardian"],
-
     # Include additional files into the package
     # include_package_data=True,
-
-    # Details
     url="https://github.com/droidlife/guardian",
-
-    #
-    # license="LICENSE.txt",
-    description="Useful towel-related stuff.",
-    install_requires=['djangorestframework']
-
-    # long_description=open("README.txt").read(),
+    license="LICENSE",
+    description='A custom django rest middleware which can be used on individual methods of APIViews. The middleware' +
+                ' includes user authentication and payload verification',
+    install_requires=['djangorestframework'],
+    long_description=open("README.md").read(),
 )
